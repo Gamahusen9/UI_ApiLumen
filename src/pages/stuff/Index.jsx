e64d0e7b03fb08223a1cd78e45d5a537a7b57b09
Gamahusen9 <gamahusein2019@gmail.com>
2024-05-17 09:33:21 +0700

    tambah sintaks

--- src/pages/stuff/Index.jsx
+++ src/pages/stuff/Index.jsx
@@ -9,6 +9,7 @@ export default function Stuff() {
     const [mess, setMess] = useState([])
     const [stat, setStat] = useState([])
 
+
     const navigate = useNavigate();
     const [error, setError] = useState([]);
     const instance = axios.create({
@@ -17,6 +18,7 @@ export default function Stuff() {
             'Authorization': 'Bearer ' + localStorage.getItem('access_token')
         }
     })
+
 
 
     useEffect(() => {
@@ -49,8 +51,50 @@ export default function Stuff() {
 
                 setMess(err.response.data.message)
                 setStat(err.response.data.success)
-                setError(err.response.data)
+                console.log(err.response.data)
             })
+    }
+    const handleDeleteStock = (id) => {
+        instance.delete(`/stock/delete/${id}`)
+            .then(res => {
+                setTimeout(() => {
+                    location.reload()
+                }, 2000);
+                setMess(res.data.message)
+                setStat(res.data.success)
+            })
+            .catch(err => {
+
+                setMess(err.response.data.message)
+                setStat(err.response.data.success)
+                console.log(err.response.data)
+            })
+    }
+
+    const handleCreateStock = (stuffid) => {
+        const data = {
+            stock: {
+                stuff_id  : stuffid,
+                total_available : 0,
+                total_defect : 0
+            }
+        }
+        console.log(stuffid);
+
+            instance.post('/stock/create', data.stock)
+                .then(res => {
+                    setTimeout(() => {
+                        location.reload()
+                    }, 500);
+                    setMess(res.data.message)
+                    setStat(res.data.success)
+                })
+                .catch(err => {
+                    // console.log(err.response.data);
+                    setMess(err.response.data.message)
+                    setStat(err.response.data.success)
+                })
+
     }
     return (
         <Case>
@@ -73,7 +117,7 @@ export default function Stuff() {
             </div>
 
 
-            <div class="relative overflow-x-auto flex items-center  m-10 mt-3 flex flex-col shadow-md rounded-lg">
+            <div class="relative overflow-x-auto flex items-center mt-3 flex flex-col shadow-md rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 rounded-lg">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -94,6 +138,9 @@ export default function Stuff() {
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Action
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Kelola stock
                             </th>
 
                         </tr>
@@ -124,9 +171,28 @@ export default function Stuff() {
                                     }
                                 </td>
                                 <td class="px-6 py-4">
-                                    <Link to={`/stuff/edit/${stuff.id}`}>                                        <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 m-2">Edit</button>
+                                    <Link to={`/stuff/edit/${stuff.id}`}>
+                                        <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 m-2">Edit</button>
                                     </Link>
-                                    <button type="button" onClick={() => deleteStuff(stuff.id)} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" >Delete</button>
+                                    {
+                                        stuff.stuffstock === null ?
+                                            <button type="button" onClick={() => deleteStuff(stuff.id)} class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" >Delete</button> : ''
+                                    }
+
+
+                                </td>
+                                <td>
+                                    {
+                                        stuff.stuffstock === null ? (
+                                            <button type="submit" onClick={() => handleCreateStock(stuff.id)} class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Tambah Stock</button>
+                                        )
+                                            : <>
+                                                <p className="text-white"><em>data stock sudah ada</em></p> <button  type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Edit</button>
+                                                <button type="button" onClick={() => handleDeleteStock(stuff.stuffstock.id)} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Delete</button>
+                                            </>
+
+
+                                    }
                                 </td>
                             </tr>
                         ))}
